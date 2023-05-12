c7aed3b36c0186eef0533885bc5608c613c2366e
Devansh <ds3835@srmist.edu.in>
2023-05-12 18:45:34 +0530

    Update arrayreversal.c

--- arrayreversal.c
+++ arrayreversal.c
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-/*    int num, *arr, i;
-    scanf("%d", &num);
-//    arr = (int*) malloc(num * sizeof(int));
-    for(i = 0; i < num; i++) {
-        scanf("%d", arr + i);
-    }
-*/
+
     int i,j,k;
     int n,m;
     scanf("%d",&n);
@@ -29,35 +23,3 @@ int main()
 
     return 0;
 }
-=======
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-/*    int num, *arr, i;
-    scanf("%d", &num);
-//    arr = (int*) malloc(num * sizeof(int));
-    for(i = 0; i < num; i++) {
-        scanf("%d", arr + i);
-    }
-*/
-    int i,j,k;
-    int n,m;
-    scanf("%d",&n);
-    int a[n];
-
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    
-    for(i=n-1;i>=0;i--)
-
-    {
-         printf("%d ",a[i]);
-    }
-
-    return 0;
-}
->>>>>>> f72bc80a7e41a14c4f2f09535f7f1b4ecf9d2126

ed6894ee881fc463c28078c4c15f79806eb1039c
Devansh <111366687+Devansh682@users.noreply.github.com>
2023-05-21 21:37:24 +0530

    corrections made

--- dynamicarray.c
+++ dynamicarray.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
- * This stores the total number of books in each shelf.
+ * This stores the total number of books in each of the shelf.
  */
 int* total_number_of_books;
 /*
